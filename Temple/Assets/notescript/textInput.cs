3dd775ea97f6cc60953cce4fe0a4207c34bce1c2
Yashika1818 <yashika124btcse20@igdtuw.ac.in>
2021-09-22 23:16:25 +0530

    finallyyyyy

--- Temple/Assets/notescript/textInput.cs
+++ Temple/Assets/notescript/textInput.cs
@@ -8,46 +8,53 @@ public class textInput : MonoBehaviour
    
   public string[] answers;
    public InputField[] inputField;
-    // public GameObject textDisplay;
-
-   // [SerializeField]
-    public GameObject[] doors;
-
    
+    public GameObject[] doors;
+    public GameObject[] DoorImg;
+
 
 
     public void OnClickClue1()
     {
        if( inputField[0].text == answers[0])
         {
-            //door 1 open 
-            // door1
-          //   doors[0].enabled = true;
+
            doors[0].SetActive(false);
+            DoorImg[0].SetActive(false);
+            inputField[0].text="";
         }
     }
-
-   // public void Clue2()
-    //{
-     //   if (inputField[1].text == answers[1])
-      /*  {
-            //door 1 open 
+    public void OnClickClue2()
+    {
+        if (inputField[1].text == answers[1])
+        {
+     
+            
+            doors[1].SetActive(false);
+            DoorImg[1].SetActive(false);
+            inputField[1].text = "";
         }
     }
-
-    public void Clue3()
+    public void OnClickClue3()
     {
         if (inputField[2].text == answers[2])
         {
-            //door 1 open 
+    
+            doors[2].SetActive(false);
+            DoorImg[2].SetActive(false);
+            inputField[2].text = "";
         }
     }
-
-    public void Clue4()
+    public void OnClickClue4()
     {
         if (inputField[3].text == answers[3])
         {
-            //door 1 open 
+          
+            doors[3].SetActive(false);
+            DoorImg[3].SetActive(false);
+            inputField[3].text = "";
         }
-    }*/
+    }
+
+   
 }
